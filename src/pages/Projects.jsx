ebcbd7d4606acc34f9e81ef242821843bb71b795
Deepanker Rajora <deepankerrajora@gmail.com>
2026-01-30 07:30:09 +0000

    File changes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Button } from "@/components/ui/button";
