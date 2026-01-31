845507097ce4eb201666a507d6273fa1a764b88d
Deepanker Rajora <deepankerrajora@gmail.com>
2026-01-31 08:54:57 +0000

    File changes

--- src/components/notifications/NotificationBell.jsx
+++ src/components/notifications/NotificationBell.jsx
@@ -112,7 +112,7 @@ export default function NotificationBell({ userEmail, notificationType = null })
                   </p>
                   <p className="text-xs text-gray-500 mt-1">{notification.message}</p>
                   <p className="text-xs text-gray-400 mt-1">
-                    {format(new Date(notification.created_date), 'MMM d, h:mm a')}
+                    {format(new Date(notification.created_date), 'MMM d, yyyy')}
                   </p>
                 </div>
               </DropdownMenuItem>
